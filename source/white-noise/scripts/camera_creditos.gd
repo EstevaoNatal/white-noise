118103be55d904fa0a0e2f8c4e745ac54ac565b6
Estevão Natal <estevaoprofissional18@gmail.com>
2026-02-28 16:01:38 -0300

    Adição musica creditos e alteração de texto em credito

--- source/white-noise/scripts/camera_creditos.gd
+++ source/white-noise/scripts/camera_creditos.gd
@@ -3,15 +3,19 @@ extends Camera2D
 @export var velocidade = 0.3
 @export var fim = 0
 @export var timer : Timer
+@export var musga : AudioStreamPlayer2D
+
 var comecar=false
 var contator = 0
 @onready var comecar_creditos: Timer = $"../comecarCreditos"
 
 func _ready() -> void:
 	comecar_creditos.start()
+	musga.play()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
+	
 	if comecar:
 		if fim > position.y:
 			position += Vector2(0,1) * velocidade
@@ -26,3 +30,7 @@ func _on_timer_timeout() -> void:
 
 func _on_comecar_creditos_timeout() -> void:
 	comecar=true
+
+
+func _on_trilha_sonora_finished() -> void:
+	musga.play()
